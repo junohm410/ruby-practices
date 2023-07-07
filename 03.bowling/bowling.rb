af1eee96fc8d023d762fccec6eb9820cc90abd68
junohm410 <junohm410@gmail.com>
2023-07-07 10:58:32 +0900

    修正版bowling.rbをコミットしました
    前回のコミットで修正前のプログラムをコミットしたため、修正版をaddしコミットしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 
 scores = score.split(',')
 
 shots = []
-scores.each do |score|
-  if score == 'X'
+scores.each do |s|
+  if s == 'X'
     shots << 10
     shots << 0
   else
-    shots << score.to_i
+    shots << s.to_i
   end
 end
 
@@ -19,25 +20,26 @@ shots.each_slice(2) do |shot|
   frames << shot
 end
 
-total_score = 0
+point = 0
 frames.each.with_index do |frame, i|
   next_frame = i + 1
   frame_after_next = i + 2
-  if i < 9
-    if frame[0] == 10
-      if frames[next_frame][0] == 10
-        total_score += 10 + frames[frame_after_next][0] + frame.sum
+  point +=
+    if i < 9
+      if frame[0] == 10
+        if frames[next_frame][0] == 10
+          10 + frames[frame_after_next][0] + frame.sum
+        else
+          frames[next_frame].sum + frame.sum
+        end
+      elsif frame.sum == 10
+        frames[next_frame][0] + frame.sum
       else
-        total_score += frames[next_frame].sum + frame.sum
+        frame.sum
       end
-    elsif frame.sum == 10
-      total_score += frames[next_frame][0] + frame.sum
     else
-      total_score += frame.sum
+      frame.sum
     end
-  else
-    total_score += frame.sum
-  end
 end
 
-puts total_score
+puts point
