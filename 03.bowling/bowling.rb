f6ff1b75f8b03f05e7bbe4e9f1d8e92e3edf4ea2
junohm410 <junohm410@gmail.com>
2023-07-14 09:42:02 +0900

    2回目のレビュー内容を受けプログラムを修正
    each内の可読性を上げるために変数名や加算の並びを変更するなど、レビュー内容を反映し修正いたしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,23 +17,23 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = frames.each.with_index.sum do |frame, i|
+point = frames.each_with_index.sum do |current_frame, i|
   next_frame = frames[i + 1]
-  frame_after_next = frames[i + 2]
+  after_next_frame = frames[i + 2]
   if i < 9
-    if frame[0] == 10
+    if current_frame[0] == 10
       if next_frame[0] == 10
-        10 + frame_after_next[0] + frame.sum
+        current_frame.sum + next_frame[0] + after_next_frame[0]
       else
-        next_frame.sum + frame.sum
+        current_frame.sum + next_frame.sum
       end
-    elsif frame.sum == 10
-      next_frame[0] + frame.sum
+    elsif current_frame.sum == 10
+      current_frame.sum + next_frame[0]
     else
-      frame.sum
+      current_frame.sum
     end
   else
-    frame.sum
+    current_frame.sum
   end
 end
 
