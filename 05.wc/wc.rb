707e9b87ab22290f5ff6bbac05c32005eb7eeb14
junohm410 <junohm410@gmail.com>
2023-08-13 23:45:40 +0900

    print_infoとprint_totalの共通部分をメソッドに切り出し。またそのために変数名やキー名を調整

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -16,22 +16,20 @@ def print_from_stdin(options)
   lines_num = count_lines(input_data_lines)
   words_num = count_words(input_data_lines)
   file_size = count_bytesize(input_data_lines)
-  total_nums = { 'total_lines' => lines_num, 'total_words' => words_num, 'total_size' => file_size }
 
-  print_total(total_nums, options)
-  print "\n"
+  lines_words_sizes = [{ 'lines_num' => lines_num, 'words_num' => words_num, 'file_size' => file_size, 'file_name' => '' }]
+  width = [lines_num, words_num, file_size].max.to_s.size
+  width = width + 1 > DEFAULT_WIDTH ? width + 1 : DEFAULT_WIDTH
+
+  print_info(lines_words_sizes, width, options)
 end
 
 def print_from_args(files, options)
   files_info = format_files_info(files)
 
-  if files.size > 1
-    print_info(files_info[:lines_words_sizes], files_info[:total_nums], options)
-    print_total(files_info[:total_nums], options)
-    print " total\n"
-  else
-    print_info(files_info[:lines_words_sizes], files_info[:total_nums], options)
-  end
+  width = calc_width(files_info[:total_nums])
+  print_info(files_info[:lines_words_sizes], width, options)
+  print_total(files_info[:total_nums], width, options) if files.size > 1
 end
 
 def format_files_info(files)
@@ -48,15 +46,15 @@ def format_files_info(files)
 
   lines_words_sizes = [lines_nums, words_nums, file_sizes, files].transpose
   lines_words_sizes_keys = %w[lines_num words_num file_size file_name]
-  formatted_3_elemetnts_info =
+  formatted_3_elements_info =
     lines_words_sizes.map { |file_info| [lines_words_sizes_keys, file_info].transpose.to_h }
 
   total_nums = [total_lines_nums, total_words_nums, total_size]
-  total_nums_keys = %w[total_lines total_words total_size]
+  total_nums_keys = %w[lines_num words_num file_size]
   formatted_total_nums = [total_nums_keys, total_nums].transpose.to_h
 
   {
-    lines_words_sizes: formatted_3_elemetnts_info,
+    lines_words_sizes: formatted_3_elements_info,
     total_nums: formatted_total_nums
   }
 end
@@ -80,27 +78,29 @@ def calc_width(nums)
   width + 1 > DEFAULT_WIDTH ? width + 1 : DEFAULT_WIDTH
 end
 
-def print_info(files, total_nums, options)
-  width = calc_width(total_nums)
+def print_info(files_data, width, options)
   output = ''
   is_option_none = options.values.none?
-  files.each do |file|
-    output += file['lines_num'].to_s.rjust(width) if options['l'] || is_option_none
-    output += file['words_num'].to_s.rjust(width) if options['w'] || is_option_none
-    output += file['file_size'].to_s.rjust(width) if options['c'] || is_option_none
-    output += " #{file['file_name']}\n"
+  files_data.each do |file_data|
+    output = arrange_each_file(output, file_data, width, options, is_option_none)
+    output += " #{file_data['file_name']}\n"
   end
   print output
 end
 
-def print_total(total_nums, options)
-  width = calc_width(total_nums)
+def print_total(total_nums, width, options)
   output = ''
   is_option_none = options.values.none?
-  output += total_nums['total_lines'].to_s.rjust(width) if options['l'] || is_option_none
-  output += total_nums['total_words'].to_s.rjust(width) if options['w'] || is_option_none
-  output += total_nums['total_size'].to_s.rjust(width) if options['c'] || is_option_none
+  output = arrange_each_file(output, total_nums, width, options, is_option_none)
+  output += " total\n"
   print output
+end
+
+def arrange_each_file(output, file_data, width, options, is_option_none)
+  output += file_data['lines_num'].to_s.rjust(width) if options['l'] || is_option_none
+  output += file_data['words_num'].to_s.rjust(width) if options['w'] || is_option_none
+  output += file_data['file_size'].to_s.rjust(width) if options['c'] || is_option_none
+  output
 end
 
 main
