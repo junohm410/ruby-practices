714b462bd5f781def723017c58a06a4c4f04e10c
junohm410 <junohm410@gmail.com>
2023-08-14 12:11:01 +0900

    抽象的なメソッド名・引数名の修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -7,62 +7,58 @@ DEFAULT_WIDTH = 8
 
 def main
   options = ARGV.getopts('lwc')
-  ARGV.none? ? print_from_stdin(options) : print_from_args(ARGV, options)
+  ARGV.none? ? print_pipelined_input(options) : print_specified_files(ARGV, options)
 end
 
-def print_from_stdin(options)
+def print_pipelined_input(options)
   input_data_lines = $stdin.readlines
 
-  lines_num = count_lines(input_data_lines)
-  words_num = count_words(input_data_lines)
+  line_count = count_lines(input_data_lines)
+  word_count = count_words(input_data_lines)
   file_size = count_bytesize(input_data_lines)
 
-  lines_words_sizes = [{ 'lines_num' => lines_num, 'words_num' => words_num, 'file_size' => file_size, 'file_name' => '' }]
-  largest_total_num_digits = [lines_num, words_num, file_size].max.to_s.size
-  width = calc_width(largest_total_num_digits)
+  input_data_properties = [{ 'line_count' => line_count, 'word_count' => word_count, 'file_size' => file_size, 'file_name' => '' }]
+  largest_total_count_digits = [line_count, word_count, file_size].max.to_s.size
+  width = calc_width(largest_total_count_digits)
 
-  print_info(lines_words_sizes, width, options)
+  print_files_own_properties(input_data_properties, width, options)
 end
 
-def print_from_args(files, options)
-  files_info = format_files_info(files)
+def print_specified_files(files, options)
+  files_info = format_files_own_properties(files)
 
-  largest_total_num_digits = files_info[:total_nums].values.max.to_s.size
-  width = calc_width(largest_total_num_digits)
+  largest_total_count_digits = files_info[:total_counts].values.max.to_s.size
+  width = calc_width(largest_total_count_digits)
 
-  print_info(files_info[:lines_words_sizes], width, options)
-  print_total(files_info[:total_nums], width, options) if files.size > 1
+  print_files_own_properties(files_info[:unique_properties], width, options)
+  print_total_counts(files_info[:total_counts], width, options) if files.size > 1
 end
 
-def format_files_info(files)
+def format_files_own_properties(files)
   all_files_lines = files.map { |file| File.open(file, &:readlines) }
 
-  lines_nums = all_files_lines.map { |file_lines| count_lines(file_lines) }
-  total_lines_nums = lines_nums.sum
+  line_counts = all_files_lines.map { |file_lines| count_lines(file_lines) }
+  total_line_count = line_counts.sum
 
-  words_nums = all_files_lines.map { |file_lines| count_words(file_lines) }
-  total_words_nums = words_nums.sum
+  word_counts = all_files_lines.map { |file_lines| count_words(file_lines) }
+  total_word_count = word_counts.sum
 
   file_sizes = all_files_lines.map { |file_lines| count_bytesize(file_lines) }
   total_size = file_sizes.sum
 
-  lines_words_sizes = [lines_nums, words_nums, file_sizes, files].transpose
-  # lines_words_sizes_keys = %w[lines_num words_num file_size file_name]
-  # formatted_3_elements_info =
-  #   lines_words_sizes.map { |file_info| [lines_words_sizes_keys, file_info].transpose.to_h }
-  # TODO: 下記の修正の方向性が正しいか質問
-  formatted_3_elements_info =
-    lines_words_sizes.map do |file_info|
-      { 'lines_num' => file_info[0], 'words_num' => file_info[1], 'file_size' => file_info[2], 'file_name' => file_info[3] }
+  files_own_properties_sets = [line_counts, word_counts, file_sizes, files].transpose
+  formatted_files_own_properties_sets =
+    files_own_properties_sets.map do |file_properties|
+      { 'line_count' => file_properties[0], 'word_count' => file_properties[1], 'file_size' => file_properties[2], 'file_name' => file_properties[3] }
     end
 
-  total_nums = [total_lines_nums, total_words_nums, total_size]
-  total_nums_keys = %w[lines_num words_num file_size]
-  formatted_total_nums = [total_nums_keys, total_nums].transpose.to_h
+  total_counts = [total_line_count, total_word_count, total_size]
+  total_counts_keys = %w[line_count word_count file_size]
+  formatted_total_counts = [total_counts_keys, total_counts].transpose.to_h
 
   {
-    lines_words_sizes: formatted_3_elements_info,
-    total_nums: formatted_total_nums
+    unique_properties: formatted_files_own_properties_sets,
+    total_counts: formatted_total_counts
   }
 end
 
@@ -84,26 +80,24 @@ def calc_width(num)
   num + 1 > DEFAULT_WIDTH ? num + 1 : DEFAULT_WIDTH
 end
 
-def print_info(files_data, width, options)
-  files_data.each do |file_data|
-    output = arrange_each_file(file_data, width, options)
-    output += " #{file_data['file_name']}"
-    puts output
+def print_files_own_properties(files_properties, width, options)
+  files_properties.each do |file_properties|
+    output = arrange_output_line(file_properties, width, options)
+    puts output + " #{file_properties['file_name']}"
   end
 end
 
-def print_total(total_nums, width, options)
-  output = arrange_each_file(total_nums, width, options)
-  output += ' total'
-  puts output
+def print_total_counts(total_counts, width, options)
+  output = arrange_output_line(total_counts, width, options)
+  puts "#{output} total"
 end
 
-def arrange_each_file(file_data, width, options)
+def arrange_output_line(properties, width, options)
   is_option_none = options.values.none?
   output = ''
-  output += file_data['lines_num'].to_s.rjust(width) if options['l'] || is_option_none
-  output += file_data['words_num'].to_s.rjust(width) if options['w'] || is_option_none
-  output += file_data['file_size'].to_s.rjust(width) if options['c'] || is_option_none
+  output += properties['line_count'].to_s.rjust(width) if options['l'] || is_option_none
+  output += properties['word_count'].to_s.rjust(width) if options['w'] || is_option_none
+  output += properties['file_size'].to_s.rjust(width) if options['c'] || is_option_none
   output
 end
 
