6d38a0ac51ba7a38fb2392c0866166b9e7127cc9
junohm410 <junohm410@gmail.com>
2023-08-14 10:21:41 +0900

    出力する処理・幅計算の共通化する内容を見直し / ファイル情報のハッシュ変換の修正案を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -18,8 +18,8 @@ def print_from_stdin(options)
   file_size = count_bytesize(input_data_lines)
 
   lines_words_sizes = [{ 'lines_num' => lines_num, 'words_num' => words_num, 'file_size' => file_size, 'file_name' => '' }]
-  width = [lines_num, words_num, file_size].max.to_s.size
-  width = width + 1 > DEFAULT_WIDTH ? width + 1 : DEFAULT_WIDTH
+  largest_total_num_digits = [lines_num, words_num, file_size].max.to_s.size
+  width = calc_width(largest_total_num_digits)
 
   print_info(lines_words_sizes, width, options)
 end
@@ -27,7 +27,9 @@ end
 def print_from_args(files, options)
   files_info = format_files_info(files)
 
-  width = calc_width(files_info[:total_nums])
+  largest_total_num_digits = files_info[:total_nums].values.max.to_s.size
+  width = calc_width(largest_total_num_digits)
+
   print_info(files_info[:lines_words_sizes], width, options)
   print_total(files_info[:total_nums], width, options) if files.size > 1
 end
@@ -45,9 +47,14 @@ def format_files_info(files)
   total_size = file_sizes.sum
 
   lines_words_sizes = [lines_nums, words_nums, file_sizes, files].transpose
-  lines_words_sizes_keys = %w[lines_num words_num file_size file_name]
+  # lines_words_sizes_keys = %w[lines_num words_num file_size file_name]
+  # formatted_3_elements_info =
+  #   lines_words_sizes.map { |file_info| [lines_words_sizes_keys, file_info].transpose.to_h }
+  # TODO: 下記の修正の方向性が正しいか質問
   formatted_3_elements_info =
-    lines_words_sizes.map { |file_info| [lines_words_sizes_keys, file_info].transpose.to_h }
+    lines_words_sizes.map do |file_info|
+      { 'lines_num' => file_info[0], 'words_num' => file_info[1], 'file_size' => file_info[2], 'file_name' => file_info[3] }
+    end
 
   total_nums = [total_lines_nums, total_words_nums, total_size]
   total_nums_keys = %w[lines_num words_num file_size]
@@ -73,30 +80,27 @@ def count_bytesize(lines)
   lines.join.bytesize
 end
 
-def calc_width(nums)
-  width = nums.values.map { |num| num.to_s.size }.max
-  width + 1 > DEFAULT_WIDTH ? width + 1 : DEFAULT_WIDTH
+def calc_width(num)
+  num + 1 > DEFAULT_WIDTH ? num + 1 : DEFAULT_WIDTH
 end
 
 def print_info(files_data, width, options)
-  output = ''
-  is_option_none = options.values.none?
   files_data.each do |file_data|
-    output = arrange_each_file(output, file_data, width, options, is_option_none)
-    output += " #{file_data['file_name']}\n"
+    output = arrange_each_file(file_data, width, options)
+    output += " #{file_data['file_name']}"
+    puts output
   end
-  print output
 end
 
 def print_total(total_nums, width, options)
-  output = ''
-  is_option_none = options.values.none?
-  output = arrange_each_file(output, total_nums, width, options, is_option_none)
-  output += " total\n"
-  print output
+  output = arrange_each_file(total_nums, width, options)
+  output += ' total'
+  puts output
 end
 
-def arrange_each_file(output, file_data, width, options, is_option_none)
+def arrange_each_file(file_data, width, options)
+  is_option_none = options.values.none?
+  output = ''
   output += file_data['lines_num'].to_s.rjust(width) if options['l'] || is_option_none
   output += file_data['words_num'].to_s.rjust(width) if options['w'] || is_option_none
   output += file_data['file_size'].to_s.rjust(width) if options['c'] || is_option_none
