19d0f7447a5f9478e4e28e097a9764c20861a5c5
junohm410 <junohm410@gmail.com>
2023-08-14 23:13:00 +0900

    変数名・メソッド名・不要なメソッドや行程の見直し・削除

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -15,50 +15,41 @@ def print_pipelined_input(options)
 
   line_count = count_lines(input_data_lines)
   word_count = count_words(input_data_lines)
-  file_size = count_bytesize(input_data_lines)
+  bytesize = count_bytesize(input_data_lines)
 
-  input_data_properties = [{ line_count:, word_count:, file_size:, file_name: '' }]
+  input_data_props = [{ line_count:, word_count:, bytesize:, file_name: '' }]
 
-  print_files_own_properties(input_data_properties, options)
+  print_file_props(input_data_props, options)
 end
 
 def print_specified_files(files, options)
-  files_info = format_files_own_properties(files)
-
-  print_files_own_properties(files_info, options)
-end
-
-def format_files_own_properties(files)
-
-  formatted_files_own_properties_sets =
+  file_props =
     files.map do |file|
       file_lines = File.open(file, &:readlines)
       line_count = count_lines(file_lines)
       word_count = count_words(file_lines)
-      file_size = count_bytesize(file_lines)
-      file_name = file
-      { line_count:, word_count:, file_size:, file_name: }
+      bytesize = count_bytesize(file_lines)
+      { line_count:, word_count:, bytesize:, file_name: file }
     end
-  
+
   if files.size > 1
     line_count = 0
     word_count = 0
-    file_size = 0
-    file_name = 'total'
-    formatted_files_own_properties_sets.each do |file|
-      line_count += file[:line_count]
-      word_count += file[:word_count]
-      file_size += file[:file_size]
+    bytesize = 0
+    file_props.each do |file_prop|
+      line_count += file_prop[:line_count]
+      word_count += file_prop[:word_count]
+      bytesize += file_prop[:bytesize]
     end
-    formatted_files_own_properties_sets << { line_count:, word_count:, file_size:, file_name: }
+    file_props << { line_count:, word_count:, bytesize:, file_name: 'total' }
   end
 
-  formatted_files_own_properties_sets
+  print_file_props(file_props, options)
 end
 
 def count_lines(lines)
-  lines_num = lines.size
-  lines.none? || lines.last.end_with?("\n") ? lines_num : lines_num - 1
+  line_count = lines.size
+  lines.none? || lines.last.end_with?("\n") ? line_count : line_count - 1
 end
 
 def count_words(lines)
@@ -69,20 +60,20 @@ def count_bytesize(lines)
   lines.join.bytesize
 end
 
-def print_files_own_properties(files_properties, options)
-  files_properties.each do |file_properties|
-    output = arrange_output_line(file_properties, options)
-    puts output + " #{file_properties[:file_name]}"
+def print_file_props(file_props, options)
+  file_props.each do |file_prop|
+    arranged_output_line = arrange_output_line(file_prop, options)
+    puts arranged_output_line + " #{file_prop[:file_name]}"
   end
 end
 
-def arrange_output_line(properties, options)
+def arrange_output_line(file_prop, options)
   is_option_none = options.values.none?
-  output = ''
-  output += properties[:line_count].to_s.rjust(PRINTING_WIDTH) if options['l'] || is_option_none
-  output += properties[:word_count].to_s.rjust(PRINTING_WIDTH) if options['w'] || is_option_none
-  output += properties[:file_size].to_s.rjust(PRINTING_WIDTH) if options['c'] || is_option_none
-  output
+  output_line = ''
+  output_line += file_prop[:line_count].to_s.rjust(PRINTING_WIDTH) if options['l'] || is_option_none
+  output_line += file_prop[:word_count].to_s.rjust(PRINTING_WIDTH) if options['w'] || is_option_none
+  output_line += file_prop[:bytesize].to_s.rjust(PRINTING_WIDTH) if options['c'] || is_option_none
+  output_line
 end
 
 main
