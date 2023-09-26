e33241cea47fddce307af7fc283bb1bec7c811b2
junohm410 <junohm410@gmail.com>
2023-09-26 17:39:50 +0900

    結果の文字列からフレームに分ける処理をFrameのクラスメソッドに移動

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,6 +3,9 @@
 require_relative 'shot'
 
 class Frame
+  FINAL_FRAME = 10
+  SHOTS_PER_NORMAL_FRAME = 2
+
   attr_reader :first_shot, :second_shot
 
   def initialize(first_fallen_pin, second_fallen_pin = nil, third_fallen_pin = nil)
@@ -21,5 +24,29 @@ class Frame
 
   def spare?
     [first_shot, second_shot].sum(&:score) == 10 && !strike?
+  end
+
+  def self.organize_all_frames(game_result)
+    fallen_pins = game_result.split(',')
+    frames = []
+    same_frame_pins = []
+    fallen_pins.each do |fallen_pin|
+      current_frame = frames.size + 1
+      Frame.organize_same_frame_pins(fallen_pin, same_frame_pins, current_frame)
+      if frames.size < (FINAL_FRAME - 1) && same_frame_pins.size == SHOTS_PER_NORMAL_FRAME
+        frames << Frame.new(*same_frame_pins)
+        same_frame_pins = []
+      end
+    end
+    frames << Frame.new(*same_frame_pins)
+  end
+
+  def self.organize_same_frame_pins(fallen_pin, same_frame_pins, current_frame)
+    if fallen_pin == 'X'
+      same_frame_pins << '10'
+      same_frame_pins << nil if current_frame < FINAL_FRAME
+    else
+      same_frame_pins << fallen_pin
+    end
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -3,29 +3,15 @@
 require_relative 'frame'
 
 class Game
-  FINAL_FRAME = 10
-  SHOTS_PER_NORMAL_FRAME = 2
-
   def initialize(game_result)
-    fallen_pins = game_result.split(',')
-    @frames = []
-    same_frame_pins = []
-    fallen_pins.each do |fallen_pin|
-      current_frame = @frames.size + 1
-      organize_same_frame_pins(fallen_pin, same_frame_pins, current_frame)
-      if @frames.size < (FINAL_FRAME - 1) && same_frame_pins.size == SHOTS_PER_NORMAL_FRAME
-        @frames << Frame.new(*same_frame_pins)
-        same_frame_pins = []
-      end
-    end
-    @frames << Frame.new(*same_frame_pins)
+    @frames = Frame.organize_all_frames(game_result)
   end
 
   def score
     @frames.each_with_index.sum do |current_frame, i|
       next_frame = @frames[i + 1]
       after_next_frame = @frames[i + 2]
-      if i < (FINAL_FRAME - 1)
+      if i < (Frame::FINAL_FRAME - 1)
         if current_frame.strike?
           calculate_strike_bonus(current_frame, next_frame, after_next_frame, i)
         elsif current_frame.spare?
@@ -42,7 +28,7 @@ class Game
   private
 
   def calculate_strike_bonus(current_frame, next_frame, after_next_frame, index)
-    return current_frame.score + next_frame.first_shot.score + next_frame.second_shot.score if index == (FINAL_FRAME - 2)
+    return current_frame.score + next_frame.first_shot.score + next_frame.second_shot.score if index == (Frame::FINAL_FRAME - 2)
 
     if next_frame.strike?
       current_frame.score + next_frame.score + after_next_frame.first_shot.score
@@ -53,14 +39,5 @@ class Game
 
   def calculate_spare_bonus(current_frame, next_frame)
     current_frame.score + next_frame.first_shot.score
-  end
-
-  def organize_same_frame_pins(fallen_pin, same_frame_pins, current_frame)
-    if fallen_pin == 'X'
-      same_frame_pins << '10'
-      same_frame_pins << nil if current_frame < FINAL_FRAME
-    else
-      same_frame_pins << fallen_pin
-    end
   end
 end
