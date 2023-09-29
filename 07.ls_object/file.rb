c7b6092a647bee4325ce429a1e6f8ada3dc49274
junohm410 <junohm410@gmail.com>
2023-09-29 19:48:50 +0900

    導出可能な属性をインスタンス変数にもたせないように修正 / 命名の見直し

--- 07.ls_object/file.rb
+++ 07.ls_object/file.rb
@@ -11,28 +11,28 @@ class OwnedFile
   GROUP = 1
   OTHER_USER = 2
 
-  attr_reader :name, :hard_links_count, :owner, :group, :file_size, :updated_at, :block_size, :file_mode
+  attr_reader :name
 
   def initialize(file_name)
-    @file_stat = File.symlink?(file_name) ? File.lstat(file_name) : File.stat(file_name)
-
     @name = file_name
-    @hard_links_count = @file_stat.nlink
-    @owner = Etc.getpwuid(@file_stat.uid).name
-    @group = Etc.getgrgid(@file_stat.gid).name
-    @file_size = @file_stat.chardev? || @file_stat.blockdev? ? device_file_num : @file_stat.size
-    @updated_at = @file_stat.mtime
-    @block_size = @file_stat.blocks
-    @file_mode = @file_stat.mode
+    @file_stat = File.symlink?(file_name) ? File.lstat(file_name) : File.stat(file_name)
   end
 
+  def block_size = @file_stat.blocks
+  def file_mode = @file_stat.mode
+  def hard_links_count = @file_stat.nlink
+  def owner = Etc.getpwuid(@file_stat.uid).name
+  def group = Etc.getgrgid(@file_stat.gid).name
+  def file_size = @file_stat.chardev? || @file_stat.blockdev? ? format_device_file_num : @file_stat.size
+  def updated_at = @file_stat.mtime
+
   def format_file_mode
-    octal_mode = @file_mode.to_s(8)
+    octal_mode = file_mode.to_s(8)
     octal_mode.insert(0, '0') if octal_mode.size == 5
     file_type = octal_mode[0..1]
     special_authority = octal_mode[2]
     rwx_modes = octal_mode[3..5]
-    format_file_type(file_type) + format_right(rwx_modes, special_authority)
+    format_file_type(file_type) + format_file_right(rwx_modes, special_authority)
   end
 
   def read_link
@@ -45,7 +45,7 @@ class OwnedFile
 
   private
 
-  def device_file_num
+  def format_device_file_num
     major_num = @file_stat.rdev_major.to_s(16)
     minor_num = @file_stat.rdev_minor.to_s(16)
     minor_num.insert(0, '0') if minor_num.size == 1
@@ -64,21 +64,21 @@ class OwnedFile
     }[file_type]
   end
 
-  def format_right(modes, authority)
-    formatted_right = ''
-    modes.each_char.with_index do |mode, i|
-      right = ''
-      right += format_mode(mode)
+  def format_file_right(rwx_modes, special_authority)
+    formatted_file_right = ''
+    rwx_modes.each_char.with_index do |rwx_mode, i|
+      file_right = ''
+      file_right += format_mode(rwx_mode)
 
-      right = set_sugid(right) if authority == SUID && i == OWNER
-      right = set_sugid(right) if authority == SGID && i == GROUP
-      right = set_sticky_bit(right) if authority == STICKY_BIT && i == OTHER_USER
-      formatted_right += right
+      file_right = apply_suid_and_sgid(file_right) if special_authority == SUID && i == OWNER
+      file_right = apply_suid_and_sgid(file_right) if special_authority == SGID && i == GROUP
+      file_right = apply_sticky_bit(file_right) if special_authority == STICKY_BIT && i == OTHER_USER
+      formatted_file_right += file_right
     end
-    formatted_right
+    formatted_file_right
   end
 
-  def format_mode(mode)
+  def format_mode(rwx_mode)
     {
       '0' => '---',
       '1' => '--x',
@@ -88,14 +88,14 @@ class OwnedFile
       '5' => 'r-x',
       '6' => 'rw-',
       '7' => 'rwx'
-    }[mode]
+    }[rwx_mode]
   end
 
-  def set_sugid(right)
-    right[-1] == 'x' ? right.gsub(/x$/, 's') : right.gsub(/-$/, 'S')
+  def apply_suid_and_sgid(file_right)
+    file_right[-1] == 'x' ? file_right.gsub(/x$/, 's') : file_right.gsub(/-$/, 'S')
   end
 
-  def set_sticky_bit(right)
-    right[-1] == 'x' ? right.gsub(/x$/, 't') : right.gsub(/-$/, 'T')
+  def apply_sticky_bit(file_right)
+    file_right[-1] == 'x' ? file_right.gsub(/x$/, 't') : file_right.gsub(/-$/, 'T')
   end
 end
--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -12,90 +12,82 @@ class LsCommand
     file_names = Dir.glob('*', flag)
     file_names = file_names.reverse if @options['r']
     @files = file_names.map { |file_name| OwnedFile.new(file_name) }
-    @width_file_name = printing_width(file_names)
+  end
+
+  def self.find_longest_string_length(file_prop_strings)
+    file_prop_strings.map(&:size).max
   end
 
   def display_files
-    return print_files_l if @options['l']
+    return display_files_in_detail if @options['l']
 
-    files = format_files(number_of_displayed_rows)
-    print_files(files, @width_file_name)
-  end
-
-  private
-
-  def printing_width(object)
-    object.map(&:size).max
-  end
-
-  def number_of_displayed_rows
-    number_of_files = @files.size
-    number_of_files.ceildiv(NUMBER_OF_COLUMNS)
-  end
-
-  def format_files(number_of_rows)
-    cols_of_files = []
-    @files.each_slice(number_of_rows) do |col_of_files|
-      cols_of_files << col_of_files
-    end
-
-    last_col = cols_of_files.last
-    last_col << '' until last_col.size == number_of_rows
-
-    cols_of_files.transpose
-  end
-
-  def print_files(formatted_file, width)
-    formatted_file.each do |files|
-      files.each do |file|
-        if file == ''
-          print file
-        else
-          print "#{file.name.ljust(width)} "
-        end
-      end
+    formatted_files = format_files(count_displayed_rows)
+    file_names = @files.map(&:name)
+    file_name_width = LsCommand.find_longest_string_length(file_names)
+    formatted_files.each do |row_files|
+      row_files.each { |file| file == '' ? print(file) : print("#{file.name.ljust(file_name_width)} ") }
       print "\n"
     end
   end
 
-  def print_files_l
-    width_uname = printing_width(user_names)
-    width_gname = printing_width(group_names)
-    width_link = printing_width(hard_links)
-    width_size = printing_width(file_sizes)
+  private
 
-    print "total #{total_blocks}\n"
+  def format_files(rows_count)
+    files_cols = []
+    @files.each_slice(rows_count) do |files_col|
+      files_cols << files_col
+    end
+
+    last_col = files_cols.last
+    last_col << '' until last_col.size == rows_count
+
+    files_cols.transpose
+  end
+
+  def count_displayed_rows
+    files_count = @files.size
+    files_count.ceildiv(NUMBER_OF_COLUMNS)
+  end
+
+  def display_files_in_detail
+    hard_links_count_width = LsCommand.find_longest_string_length(organize_all_hard_links_counts)
+    owner_width = LsCommand.find_longest_string_length(organize_all_owners)
+    group_width = LsCommand.find_longest_string_length(organize_all_groups)
+    file_size_width = LsCommand.find_longest_string_length(organize_all_file_sizes)
+
+    print "total #{calculate_total_block_sizes}\n"
     @files.each do |file|
-      mode = file.format_file_mode
-      link = file.hard_links_count.to_s
-      size = file.file_size.to_s
-      updated_at = file.updated_at.strftime('%-m %_d %H:%M')
+      formatted_file_mode = file.format_file_mode
+      hard_links_count = file.hard_links_count.to_s
       owner = file.owner
       group = file.group
+      file_size = file.file_size.to_s
+      updated_at = file.updated_at.strftime('%-m %_d %H:%M')
 
-      print "#{mode}  #{link.rjust(width_link)} #{owner.ljust(width_uname)}  #{group.rjust(width_gname)}  #{size.rjust(width_size)}  #{updated_at} #{file.name}"
+      print "#{formatted_file_mode}  #{hard_links_count.rjust(hard_links_count_width)} #{owner.ljust(owner_width)}  "
+      print "#{group.rjust(group_width)}  #{file_size.rjust(file_size_width)}  #{updated_at} #{file.name}"
       print " -> #{file.read_link}" if file.symbolic_link?
       print "\n"
     end
   end
 
-  def user_names
-    @files.map(&:owner)
-  end
-
-  def group_names
-    @files.map(&:group)
-  end
-
-  def hard_links
+  def organize_all_hard_links_counts
     @files.map { |file| file.hard_links_count.to_s }
   end
 
-  def file_sizes
+  def organize_all_owners
+    @files.map(&:owner)
+  end
+
+  def organize_all_groups
+    @files.map(&:group)
+  end
+
+  def organize_all_file_sizes
     @files.map { |file| file.file_size.to_s }
   end
 
-  def total_blocks
+  def calculate_total_block_sizes
     @files.sum(&:block_size)
   end
 end
